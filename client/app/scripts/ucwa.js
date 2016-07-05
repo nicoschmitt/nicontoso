fb08374ce46b38eef8161529a1ab7c78d996faf9
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-07-05 16:37:54 +0200

    display meeting creation

--- client/app/scripts/ucwa.js
+++ client/app/scripts/ucwa.js
@@ -17,11 +17,16 @@
     app.controller('ucwaCtrl', ['$http', "$location", "adalAuthenticationService",
         function ($http, $location, adal) {
             var vm = this;
+            vm.loading = true;
+            vm.message = "";
+            vm.joinUrl = "";
             
             vm.isAuthenticated = function() { return adal.userInfo.isAuthenticated }
 
             function httpError(response) {
                 console.log(response.data || "Request failed");
+                vm.message = response.data || "Request failed";
+                vm.loading = false;
             }
 
             function autoDiscover(url, callback) {
@@ -43,27 +48,35 @@
                 }, httpError);
             }
         
+            vm.message = "autodiscover...";
             autoDiscover("https://webdir.online.lync.com/autodiscover/autodiscoverservice.svc/root", function(appurl) {
+                vm.message = "register app...";
+
                 var app = {
                     "UserAgent":"node",
                     "EndpointId":"94e193bc-0ef8-4ea4-bcda-a6cb0d5c91c4",
                     "Culture":"en-US",
                 };
+
                 $http.post(appurl, app).then(function(response) {
+                    vm.message = "create meeting...";
+
                     var meetings = reduceUrlToHost(appurl) + response.data._embedded.onlineMeetings._links.myOnlineMeetings.href;
 
-                    var current = adal.userInfo.userName;
-                    
                     var newmeeting = {
-                        "attendanceAnnouncementsStatus":"Disabled",
-                        "description":"",
-                        "subject":"Quick Meeting",
-                        "attendees":[],
-                        "leaders":["sip:" + current]
+                        "attendanceAnnouncementsStatus": "Disabled",
+                        "description": "",
+                        "subject": "Quick Meeting",
+                        "attendees": [],
+                        "leaders": ["sip:" + adal.userInfo.userName]
                     };
 
                     $http.post(meetings, newmeeting).then(function(response) {
-                        console.log(response.data.joinUrl);
+                        vm.message = "";
+                        vm.loading = false;
+
+                        console.log(response.data);
+                        vm.joinUrl = response.data.joinUrl;
                     }, httpError);
 
                 }, httpError);
