7a6d9dffd4bcd0e2bd7d0e8a91f57e76d963c1bf
Nicolas Schmitt <nicolas.schmitt@gmail.com>
2016-07-23 10:15:11 +0200

    don't reload full app during login

--- client/app/app.js
+++ client/app/app.js
@@ -1,41 +1,58 @@
 (function() {
-    var app = angular.module('myApp', [ 'ngRoute', "AdalAngular", "ngMaterial", "chart.js", 'angulartics', 'angulartics.google.analytics' ]);
-  
-    app.config(["$config", '$routeProvider', '$httpProvider', "adalAuthenticationServiceProvider",
-        function ($config, $routeProvider, $httpProvider, adalProvider) {
+    var app = null;
 
-            $routeProvider.when("/Home", {
-                templateUrl: "/app/views/home.html",
-                controller: "homeCtrl",
-                controllerAs: "vm"
+    if (window !== window.parent) {
+
+        // dummy app to use during login process
+        console.log("dummy app for login");
+        app = angular.module("myApp", [ "AdalAngular", "angulartics" ])
+                    .config(["$config", '$httpProvider', 'adalAuthenticationServiceProvider', function ($config, $httpProvider, adalProvider) {
+                        adalProvider.init({
+                            clientId: $config.adalAppId,
+                        }, $httpProvider);
+                    }]);
+
+    } else {
+
+        // real application
+        var app = angular.module('myApp', [ 'ngRoute', "AdalAngular", "ngMaterial", "chart.js", 'angulartics', 'angulartics.google.analytics' ]);
+        app.config(["$config", '$routeProvider', '$httpProvider', "adalAuthenticationServiceProvider",
+            function ($config, $routeProvider, $httpProvider, adalProvider) {
+
+                $routeProvider.when("/Home", {
+                    templateUrl: "/app/views/home.html",
+                    controller: "homeCtrl",
+                    controllerAs: "vm"
+                    
+                }).when("/Mic", {
+                    requireADLogin: true,
+                    templateUrl: "/app/views/mic.html",
+                    controller: "micCtrl",
+                    controllerAs: "vm"
+
+                }).when("/UCWA", {
+                    requireADLogin: true,
+                    templateUrl: "/app/views/ucwa.html",
+                    controller: "ucwaCtrl",
+                    controllerAs: "vm"
+
+                }).when("/Login", {
+                    requireADLogin: true,
+                    redirectTo: "/Home"
+
+                }).otherwise({ redirectTo: "/Home" });
                 
-            }).when("/Mic", {
-                requireADLogin: true,
-                templateUrl: "/app/views/mic.html",
-                controller: "micCtrl",
-                controllerAs: "vm"
+                adalProvider.init({
+                    tenant: "common",
+                    clientId: $config.adalAppId,
+                    anonymousEndpoints: [ "/views", "/api/config", "/api/tenant" ],
+                    endpoints: {
+                        "https://webdir.online.lync.com": "https://webdir.online.lync.com"
+                    }
+                }, $httpProvider);
+        }]);
 
-            }).when("/UCWA", {
-                requireADLogin: true,
-                templateUrl: "/app/views/ucwa.html",
-                controller: "ucwaCtrl",
-                controllerAs: "vm"
-
-            }).when("/Login", {
-                requireADLogin: true,
-                redirectTo: "/Home"
-
-            }).otherwise({ redirectTo: "/Home" });
-            
-            adalProvider.init({
-                tenant: "common",
-                clientId: $config.adalAppId,
-                anonymousEndpoints: [ "/views", "/api/config", "/api/tenant" ],
-                endpoints: {
-                    "https://webdir.online.lync.com": "https://webdir.online.lync.com"
-                }
-            }, $httpProvider);
-    }]);
+    }
    
     fetchData().then(launchApplication);
 
@@ -45,7 +62,7 @@
         return $http.get("/api/config").then(function(resp){
             app.constant("$config", resp.data);
             // start google analytics
-            if (ga) ga('create', resp.data.analytics, 'auto');
+            if (ga && window === window.parent) ga('create', resp.data.analytics, 'auto');
         });
     };
 
