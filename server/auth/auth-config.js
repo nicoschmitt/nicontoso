68a4da069003e618bde471a5050dd154005caa24
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-10-20 11:41:13 +0200

    update azure ad lib

--- server/auth/auth-config.js
+++ server/auth/auth-config.js
@@ -11,7 +11,7 @@
         passport.use(new SimpleStrategy());
         passport.use(new OIDCBearerStrategy({
             "identityMetadata": "https://login.microsoftonline.com/common/.well-known/openid-configuration",
-            "audience": process.env.MS_APP_ID,
+            "clientID": process.env.MS_APP_ID,
             "validateIssuer": false,
             "loggingLevel": "error"
         }, function (token, done) {
